8db4c4170cc3d3102b289b803d51a9aa014f94a6
al-aminh <alaminhossain123245@gmail.com>
2025-08-24 21:25:35 +0600

    first commit

---
+++ lib/Gridview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Gridview extends StatelessWidget {
+  const Gridview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Gridview')
+    );
+  }
+}
---
+++ lib/Listview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Listview extends StatelessWidget {
+  const Listview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Listview'),
+    );
+  }
+}
